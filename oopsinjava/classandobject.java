b7311539193fe3b945cf864457721561dd1b6e58
Sparsh Katiyar <116800593+Sparsh-katiyar@users.noreply.github.com>
2024-05-06 22:19:43 +0530

    class

---
+++ oopsinjava/classandobject.java
@@ -0,0 +1,18 @@
+
+
+package OOPSinJAVA;
+ 
+ public class StudentClass{
+    public class Student{
+        String name;
+        int rno;
+        double percent;
+    }
+    pubklic static void main(String[] args){
+        Studnet x = new Student();
+        x.name ="Sparsh  katiyar";
+        x.rno = 2101650100106;
+        x.percent= 86.00;
+        Sysytem.out.println(x.name);
+    }
+ }
